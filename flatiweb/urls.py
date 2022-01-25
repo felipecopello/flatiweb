b4ea435863dda6a758147fab4022121254e07d71
felipe <felipecopello94@gmail.com>
2022-01-25 18:11:25 +0100

    homepage setup begins

---
+++ flatiweb/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import Home
+
+urlpatterns = [
+        path(
+        'home/', 
+        Home, 
+        name="home"
+        ),
+]
--- flatiweb/views.py
+++ flatiweb/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def Home(request):
+
+    return render (
+        request,
+        'flatiweb/home.html',
+        { }
+    )
+
+
